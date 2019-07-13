e52aac858e9aa28c8bdc18174ca889659dddfa6b
Cauê Baasch de Souza <cauebs@pm.me>
2019-07-13 13:14:08 -0300

    Integrate crawler and add scheduling

--- matrufsc_api/__main__.py
+++ matrufsc_api/__main__.py
@@ -1,23 +1,41 @@
 import json
 
-from click import command, argument, Path
+from click import command, argument, option, Path
 from aiohttp.web import Application, run_app
 
-from .handlers import courses, classes
+from . import jobs, handlers
 
 
 @command()
 @argument('database', type=Path(exists=True))
 @argument('host', default='0.0.0.0')
 @argument('port', default='8080')
-def main(database: str, host: str, port: int):
+@option('-u', '--update-now', is_flag=True,
+        help='Update the database on start')
+@option('-i', '--update-interval', type=float, default=10,
+        help='Database update interval in minutes')
+def main(
+    database: str,
+    host: str,
+    port: int,
+    update_now: bool,
+    update_interval: float,
+):
     app = Application()
 
-    with open(database) as f:
-        app['database'] = json.load(f)
+    app['database_path'] = database
+    app['update_interval'] = update_interval
 
-    app.router.add_get('/courses', courses)
-    app.router.add_get('/classes', classes)
+    if update_now:
+        app.on_startup.append(jobs.start_database_update)
+
+    else:
+        with open(database) as f:
+            app['database'] = json.load(f)
+        app.on_startup.append(jobs.schedule_database_update)
+
+    app.router.add_get('/courses', handlers.courses)
+    app.router.add_get('/classes', handlers.classes)
 
     run_app(app, host=host, port=port)
 
---
+++ matrufsc_api/jobs.py
@@ -0,0 +1,32 @@
+import json
+
+from time import sleep
+
+import matrufsc_crawler as crawler
+
+from aiohttp.web import Application
+
+
+def update_database(app):
+    print('Starting database update...')
+    data = crawler.run()
+    print('Finished database update.')
+
+    app['database'] = data
+    with open(app['database_path'], 'w') as f:
+        json.dump(data, f)
+
+
+async def start_database_update(app: Application):
+    update_database(app)
+    app.loop.run_in_executor(None, repeat_database_update, app)
+
+
+async def schedule_database_update(app: Application):
+    app.loop.run_in_executor(None, repeat_database_update, app)
+
+
+def repeat_database_update(app: Application):
+    while True:
+        sleep(float(app['update_interval']) * 60)
+        update_database(app)
